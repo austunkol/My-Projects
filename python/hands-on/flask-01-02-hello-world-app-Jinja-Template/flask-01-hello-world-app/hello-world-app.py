c76c98d2ebf9893f5d195c88be44076e91c0183e
Ali Ustunkol <64160260+austunkol@users.noreply.github.com>
2021-06-15 21:25:26 -0400

    first repository

---
+++ python/hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return 'Hello World!'
+@app.route('/second')
+def second():
+    return 'No Pain No Gain'
+@app.route('/third/subthird')
+def third():
+    return 'This is the subpage of third page'
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id number of this page is {id}'
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
